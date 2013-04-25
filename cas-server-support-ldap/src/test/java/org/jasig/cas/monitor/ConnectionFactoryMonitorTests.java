119e125de1780803e040da74b8deb2cde23a0c7a
Marvin S. Addison <marvin.addison@gmail.com>
2013-04-25 15:37:08 -0400

    Add annotations to monitor unit tests for conditional execution.

--- cas-server-support-ldap/src/test/java/org/jasig/cas/monitor/ConnectionFactoryMonitorTests.java
+++ cas-server-support-ldap/src/test/java/org/jasig/cas/monitor/ConnectionFactoryMonitorTests.java
@@ -21,6 +21,7 @@ package org.jasig.cas.monitor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -34,6 +35,7 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/applicationContext-test.xml"})
+@IfProfileValue(name = "monitorConfig", value = "true")
 public class ConnectionFactoryMonitorTests {
 
     @Autowired
